1b188425b37f04c18dc8f35987d802e34d1cb3ad
Ranel Gilyazov <ranel.yes@yandex.ru>
2023-06-04 22:07:17 +0300

    fix: исправлены ошибки

--- errors/collectionOfErrors.js
+++ errors/collectionOfErrors.js
@@ -1,4 +1,3 @@
-const { InternslServerError } = require('./InternslServerError');
 const { Forbidden } = require('./Forbidden');
 const { NotFoundError } = require('./NotFoundError');
 const { BadRequest } = require('./BadRequest');
@@ -6,5 +5,5 @@ const { Conflict } = require('./Conflict');
 const { Unauthorized } = require('./Unauthorized');
 
 module.exports = {
-  InternslServerError, Forbidden, NotFoundError, BadRequest, Conflict, Unauthorized,
+  Forbidden, NotFoundError, BadRequest, Conflict, Unauthorized,
 };
